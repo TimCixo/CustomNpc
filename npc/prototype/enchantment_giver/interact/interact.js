2681a1ff2d96ccbd702e35519818b7796f951f3d
TimCixo <enderhiplol@gmail.com>
2026-04-13 17:07:04 +0300

    fix(enchantment_giver): use hardcoded list of enchantment IDs instead of registry access

--- npc/prototype/enchantment_giver/interact/interact.js
+++ npc/prototype/enchantment_giver/interact/interact.js
@@ -1,6 +1,49 @@
 var BuiltInRegistries = Java.type("net.minecraft.core.registries.BuiltInRegistries");
 var EnchantmentHelper = Java.type("net.minecraft.world.item.enchantment.EnchantmentHelper");
-var Registries = Java.type("net.minecraft.core.registries.Registries");
+var ResourceLocation = Java.type("net.minecraft.resources.ResourceLocation");
+
+var ENCHANTMENT_IDS = [
+    "minecraft:protection",
+    "minecraft:fire_protection",
+    "minecraft:feather_falling",
+    "minecraft:blast_protection",
+    "minecraft:projectile_protection",
+    "minecraft:thorns",
+    "minecraft:depth_strider",
+    "minecraft:frost_walker",
+    "minecraft:binding_curse",
+    "minecraft:soul_speed",
+    "minecraft:swift_sneak",
+    "minecraft:sharpness",
+    "minecraft:smite",
+    "minecraft:bane_of_arthropods",
+    "minecraft:knockback",
+    "minecraft:fire_aspect",
+    "minecraft:looting",
+    "minecraft:sweeping_edge",
+    "minecraft:efficiency",
+    "minecraft:silk_touch",
+    "minecraft:unbreaking",
+    "minecraft:fortune",
+    "minecraft:power",
+    "minecraft:punch",
+    "minecraft:flame",
+    "minecraft:infinity",
+    "minecraft:luck_of_the_sea",
+    "minecraft:lure",
+    "minecraft:loyalty",
+    "minecraft:impaling",
+    "minecraft:riptide",
+    "minecraft:channeling",
+    "minecraft:multishot",
+    "minecraft:piercing",
+    "minecraft:quick_charge",
+    "minecraft:wind_burst",
+    "minecraft:density",
+    "minecraft:breach",
+    "minecraft:mending",
+    "minecraft:vanishing_curse"
+];
 
 function interact(event) {
     var npc = event.npc;
@@ -19,14 +62,11 @@ function interact(event) {
     }
 
     try {
-        var registry = npc.getWorld().registryAccess().registry(Registries.ENCHANTMENT).get();
-        var iterator = registry.entrySet().iterator();
         var enchanted = false;
 
-        while (iterator.hasNext()) {
-            var entry = iterator.next();
-            var ench = entry.getValue();
-            if (ench.canEnchant(mcStack)) {
+        for (var i = 0; i < ENCHANTMENT_IDS.length; i++) {
+            var ench = BuiltInRegistries.ENCHANTMENT.get(ResourceLocation.parse(ENCHANTMENT_IDS[i]));
+            if (ench != null && ench.canEnchant(mcStack)) {
                 var maxLevel = ench.getMaxLevel();
                 EnchantmentHelper.setEnchantmentLevel(mcStack, ench, maxLevel);
                 enchanted = true;
